242759f773812ad187bc68b2a980118784d6006e
zong <2298324660@qq.com>
2018-01-05 15:46:35 +0800

    '修改app.js，设置全局变量；调用豆瓣api，完成推荐页面的加载；百度api调用存在问题，无法得到位置信息'

--- pages/board/board.js
+++ pages/board/board.js
@@ -1,1 +1,37 @@
-Page({})
+// pages/border/border.js
+'use strict'
+// 获取全局应用程序实例对象
+const app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    boards: [
+      {key: 'in_theaters'},
+      {key: 'coming_soon'},
+      {key: 'top250'}
+    ],
+    loading: true
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function onLoad() {
+    var _this = this;
+    var promises = this.data.boards.map(function (board) {
+      return app.douban.find(board.key, 1, 10)
+        .then(function (d) {
+          board.title = d.title;
+          board.movies = d.subjects;
+          return board;
+        });
+    });
+    Promise.all(promises).then(function (boards) {
+      return _this.setData({boards: boards, loading: false});
+    });
+  }
+})
