bb9a22633d3e7473558203fe5d5ecf7f12662801
zong <2298324660@qq.com>
2018-01-08 20:05:33 +0800

    '完成单个电影信息的详细内容界面，修改所有指向该界面的链接'

--- pages/item/item.js
+++ pages/item/item.js
@@ -1,1 +1,39 @@
-Page({})
+'use strict';
+
+// 获取全局应用程序实例对象
+var app = getApp();
+
+Page({
+  data: {
+    title: '',
+    loading: true,
+    movie: {}
+  },
+
+  onLoad: function onLoad(params) {
+    var _this = this;
+
+    app.douban.findOne(params.id).then(function (d) {
+      _this.setData({title: d.title, movie: d, loading: false});
+      wx.setNavigationBarTitle({
+        title: '豆瓣 >> 电影 >> ' + d.title })
+    }).catch(function (e) {
+      _this.setData({ title: '获取数据异常', movie: {}, loading: false });
+      console.error(e);
+    })
+  },
+
+  onReady: function onRead() {
+    wx.setNavigationBarTitle({
+      title: '豆瓣 >> 电影 >> ' + this.data.title
+    })
+  },
+
+  onShareAppMessage: function onShareAppMessage() {
+    return {
+      title: '自定义分享标题',
+      desc: '自定义分享描述',
+      path: '/pages/item?id=' + this.data.id
+    }
+  }
+})
