548cca951a2d43adf8aa5f9f49dd82809646b31e
zong <2298324660@qq.com>
2018-01-08 16:18:51 +0800

    '完成关于界面的内容'

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,1 +1,33 @@
-Page({})
+'use strict';
+
+// 获取全局应用程序实例对象
+var app = getApp();
+
+Page({
+  data: {
+    title: '关于',
+    userInfo: {
+      wechat: '',
+      nickName: '武汉理工大学',
+      avatarUrl: '../../images/hdImg.jpg'
+    }
+  },
+
+  getUserInfo: function getUserInfo() {
+    var _this = this;
+    app.wechat.getUserInfo().then(function (res) {
+      return _this.setData({userInfo: res.userInfo});
+    })
+  },
+
+  onLoad: function onLoad () {
+    app.wechat.login().then(function (res) {
+      if(res.code) {
+        console.log('登录成功！' + res.code);
+      }
+      else {
+        console.error('获取用户登录态失败！' + res.errMsg);
+      }
+    })
+  }
+})
